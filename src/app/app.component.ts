d3b604e93501621f3ef6d38f1c8242fa4215b180
Simone Comberiati <simonecomb@gmail.com>
2025-06-04 10:08:05 +0200

    03/06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { ColorsComponent } from "./components/colors/colors.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ColorsComponent],
+  imports: [ColorsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
